16b2f418284bdd5d573c0f17a2f82c0b84a4eed5
tarotene <tarotene@gmail.com>
2018-03-19 15:49:27 +0900

    Delete all caches.

--- sh/_calcness_2d.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-:>physquan.dat
-for beta in `cat ../../list_beta.dat`; do
-	cd "beta${beta}"
-  echo "${l_x} ${l_z} ${beta} ${vel} ${l_t} ${n_s} ${l_th} ${l_b}" | ../../../../bin/calcness_2d >> ../physquan.dat
-	echo "Done: beta = ${beta}, len_z = ${l_z}, len_x = ${l_x}, id_BC = ${id_BC}"
-	cd ../
-done
