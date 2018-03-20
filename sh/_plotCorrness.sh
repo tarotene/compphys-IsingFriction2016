c4bb325b52c5493e34e8f46755d4a133b1f16ba0
tarotene <tarotene@gmail.com>
2018-03-20 15:03:19 +0900

    [change] add "_" to plotCorness.gp ALSO in its script
    Indicate the rule that any scripts running inside DO NOT run outside.

--- sh/_plotCorrness.sh
+++ sh/_plotCorrness.sh
@@ -2,4 +2,4 @@
 
 set_s=$(seq 1 1 ${n_s} | printf "%.4d\n" $(cat))
 
-cat ../../list_beta.dat | parallel --bar gnuplot -e \"beta=\'{}\'\; l_t=\'${l_t}\'\; set_s=\'${set_s}\'\" ../../../gp/plotCorrness.gp
+cat ../../list_beta.dat | parallel --bar gnuplot -e \"beta=\'{}\'\; l_t=\'${l_t}\'\; set_s=\'${set_s}\'\" ../../../gp/_plotCorrness.gp
