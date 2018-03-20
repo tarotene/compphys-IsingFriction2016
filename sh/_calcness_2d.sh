bb34cc59aba464281f282f599d6fc93f096a8f88
tarotene <tarotene@gmail.com>
2018-03-20 21:40:17 +0900

    [fix] Insert "#" to the head of physquan*.dat (in its script).
    Forgotten commenting out.

--- sh/_calcness_2d.sh
+++ sh/_calcness_2d.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 
-echo "beta, eb, err_eb, ee, err_ee, mb, err_mb, me, err_me, pump, err_pump" >physquan1.dat
-echo "beta, cb, err_cb, ce, err_ce, chib, err_chib, chie, err_chie, ub, err_ub, ue, err_ue" >physquan2.dat
+echo "# beta, eb, err_eb, ee, err_ee, mb, err_mb, me, err_me, pump, err_pump" >physquan1.dat
+echo "# beta, cb, err_cb, ce, err_ce, chib, err_chib, chie, err_chie, ub, err_ub, ue, err_ue" >physquan2.dat
 for beta in `cat ../../list_beta.dat`; do
 	cd "beta${beta}"
   echo "${l_x} ${l_z} ${beta} ${vel} ${l_t} ${n_s} ${l_th} ${l_b}" | ../../../../bin/calcness_2d
