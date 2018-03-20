fa7208e2739e7ed0443414397db10beea5a7c054
tarotene <tarotene@gmail.com>
2018-03-20 20:53:48 +0900

    [change] output physquan*.dat ALSO in each temperature folder.
    Manage several obs's in parallel.

--- sh/_calcness_2d.sh
+++ sh/_calcness_2d.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-:>physquan.dat
+
+echo "beta, eb, err_eb, ee, err_ee, mb, err_mb, me, err_me, pump, err_pump" >physquan1.dat
+echo "beta, cb, err_cb, ce, err_ce, chib, err_chib, chie, err_chie, ub, err_ub, ue, err_ue" >physquan2.dat
 for beta in `cat ../../list_beta.dat`; do
 	cd "beta${beta}"
-	pwd
-  echo "${l_x} ${l_z} ${beta} ${vel} ${l_t} ${n_s} ${l_th} ${l_b}" | ../../../../bin/calcness_2d >> ../physquan.dat
+  echo "${l_x} ${l_z} ${beta} ${vel} ${l_t} ${n_s} ${l_th} ${l_b}" | ../../../../bin/calcness_2d
 	cd ../
+	cat "beta${beta}"/physquan1.dat >> physquan1.dat
+	cat "beta${beta}"/physquan2.dat >> physquan2.dat
 done
