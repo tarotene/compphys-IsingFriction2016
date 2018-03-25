aa234a9b2e440ccfd91931d162ac32cba3576a49
tarotene <tarotene@gmail.com>
2018-03-25 21:40:21 +0900

    [change] throw both samples and params into the parallel loop
    make its gp file as slim as possible

--- sh/_plotCorrness.sh
+++ sh/_plotCorrness.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-set_beta=$(cat ../../list_beta.dat)
-seq 1 1 ${n_s} | printf "%.4d\n" $(cat) | parallel --bar gnuplot -e \"l_t=\'${l_t}\'\; set_s=\'{}\'; set_beta=\'${set_beta}\'\" ../../../gp/_plotCorrness.gp
+for s in $(seq 1 1 ${n_s} | printf "%.4d\n" $(cat)); do
+    dir="eps_s${s}"; [ ! -e $dir ] && mkdir -p $dir
+done
+
+parallel --bar gnuplot -e \"l_t=\'${l_t}\'\; l_x=\'${l_x}\'\; l_z=\'${l_z}\'\; s=\'{2}\'\; beta=\'{1}\'\" ../../../gp/_plotCorrness.gp ::: $(cat ../../list_beta.dat) ::: $(seq 1 1 ${n_s} | printf "%.4d\n" $(cat)) 
