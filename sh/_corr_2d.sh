fd39b57f78f4f22dfbbf4b7953e7385b5548daa1
tarotene <tarotene@gmail.com>
2018-04-06 09:30:41 +0900

    [update] fix filenames and split functions
    assign each F90 to its counterpart SH

--- sh/_corr_2d.sh
+++ sh/_corr_2d.sh
@@ -3,6 +3,6 @@
 for beta in `cat ../../list_beta.dat`; do
 	cd "beta${beta}"
 	pwd
-  echo "${l_x} ${l_z} ${beta} ${vel} ${l_t} ${n_s} ${l_b}" | ../../../../../bin/corrness_2d
+  echo "${l_x} ${l_z} ${beta} ${vel} ${l_t} ${n_s} ${l_b}" | ../../../../../bin/corr_2d
 	cd ../
 done
--- sh/_deldat.sh
+++ sh/_deldat.sh
@@ -2,6 +2,13 @@
 
 for beta in $(cat ../../list_beta.dat); do
 	cd "beta${beta}"
-  rip *
+	rip eb_sweep/*
+	rip ee_sweep/*
+	rip en_step/*
+	rip m_step/*
+	rip mb_sweep/*
+	rip me_sweep/*
+	rip p_sweep/*
+	rip sp_sweep/*
 	cd ../
 done
