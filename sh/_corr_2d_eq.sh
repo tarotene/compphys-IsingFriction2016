84938a57db4fd099aacaa1f70469881639ccb070
tarotene <tarotene@gmail.com>
2018-04-09 10:50:26 +0900

    [fix] correct input params
    adjust to its prog

--- sh/_corr_2d_eq.sh
+++ sh/_corr_2d_eq.sh
@@ -3,6 +3,6 @@
 for beta in `cat ../../list_beta.dat`; do
 	cd "beta${beta}"
 	pwd
-  echo "${l_x} ${l_z} ${beta} ${vel} ${l_t} ${n_s} ${l_b}" | ../../../../../bin/corr_2d_eq
+  echo "${l_x} ${l_z} ${beta} ${l_t} ${n_s} ${l_b}" | ../../../../../bin/corr_2d_eq
 	cd ../
 done
--- sh/_corr_3d_eq.sh
+++ sh/_corr_3d_eq.sh
@@ -3,6 +3,6 @@
 for beta in `cat ../../list_beta.dat`; do
 	cd "beta${beta}"
 	pwd
-  echo "${l_x} ${l_y} ${l_z} ${beta} ${vel} ${l_t} ${n_s} ${l_b}" | ../../../../../bin/corr_3d_eq
+  echo "${l_x} ${l_y} ${l_z} ${beta} ${l_t} ${n_s} ${l_b}" | ../../../../../bin/corr_3d_eq
 	cd ../
 done
