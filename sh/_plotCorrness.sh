91d74e9488fd96dad821bd17ddc20ec1481f422f
tarotene <tarotene@gmail.com>
2018-04-09 10:49:32 +0900

    [update] divide plotac file
    make clean whole structure

--- sh/_plotCorrness.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-for s in $(seq 1 1 ${n_s} | printf "%.4d\n" $(cat)); do
-    dir="eps_s${s}"; [ ! -e $dir ] && mkdir -p $dir
-done
-
-parallel --bar gnuplot -e \"l_t=\'${l_t}\'\; l_x=\'${l_x}\'\; l_z=\'${l_z}\'\; s=\'{2}\'\; beta=\'{1}\'\" ../../../gp/_plotCorrness.gp ::: $(cat ../../list_beta.dat) ::: $(seq 1 1 ${n_s} | printf "%.4d\n" $(cat)) 
